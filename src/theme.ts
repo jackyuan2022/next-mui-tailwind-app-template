248f74ff011c5ce330da602a028c682d87ff91a1
yuanjl <yuanjl@fxiaoke.com>
2024-11-21 11:34:10 +0800

    version 1.0.0

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  cssVariables: true,
+  typography: {
+  },
+  components: {
+  },
+});
+
+export default theme;
